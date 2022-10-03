48168f33eb18fba80352a88b98235540872ef281
Gustavo de Oliveira Feliciano - 322121746 <322121746@ulife.com.br>
2022-10-03 19:33:24 -0300

    first commit

---
+++ Atividade2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 322121746
+ */
+public class Atividade2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leia = new Scanner(System.in);
+        int x;
+        System.out.println("Insira um Numero");
+        x = leia.nextInt();
+        System.out.println("Seu numero e " + x);
+        
+        
+    }
+    
+}
